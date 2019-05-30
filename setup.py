cdf9a3de2bfff72ab9ff851b515fd734cd05be38
Ian Kjos <kjosib@gmail.com>
2019-05-30 18:48:24 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+	name='glowing-chainsaw',
+	version='0.0.1',
+	packages=['chainsaw', ],
+	license='MIT',
+	description='A lazy declarative DSL for high-functioning business-oriented numerical and graphical reporting',
+	long_description=open('README.md').read(),
+	long_description_content_type="text/markdown",
+	url="https://github.com/kjosib/glowing-chainsaw",
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+		"Development Status :: 1 - Planning",
+		"Topic :: Office/Business",
+    ],
+)
