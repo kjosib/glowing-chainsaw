9484ed9b008e4ba2bc5eb3a53d585c7b461417d8
Ian Kjos <kjosib@gmail.com>
2019-06-21 07:40:27 -0500

    Organize

--- src/cubicle/cradle.py
+++ src/cubicle/cradle.py
@@ -2,5 +2,8 @@
 The "cradle" is where things get started. Not the simplest thing that might work,
 but the simplest thing that does any work whatsoever.
 """
-
+import os
 from boozetools import foundation
+from boozetools.macroparse import compiler
+
+print(open(os.path.join(os.path.split(__file__)[0], 'grammar.md')).read())
