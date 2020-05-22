b2d8e1cd17cab0dd019a024505451774446f5d3b
Ian Kjos <kjosib@gmail.com>
2020-05-21 22:03:48 -0500

    Move setup script where it belongs

--- setup.py
+++ setup.py
@@ -1,24 +1,32 @@
+"""
+Packaging script for PyPI.
+"""
+
 import setuptools
 
 setuptools.setup(
 	name='cubicle',
+	author='Ian Kjos',
+	author_email='kjosib@gmail.com',
 	include_package_data=True,
 	version='0.0.1',
 	packages=['cubicle', ],
+	package_dir = {'': 'src'},
 	license='MIT',
 	description='A declarative DSL for high-functioning business-oriented tabular and hierarchical reporting',
 	long_description=open('README.md').read(),
 	long_description_content_type="text/markdown",
-	url="https://github.com/kjosib/project-cubicle",
+	url="https://github.com/kjosib/glowing-chainsaw",
 	classifiers=[
 		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
-		"Development Status :: 2 - Pre-Alpha",
+		"Development Status :: 3 - Alpha",
 		"Intended Audience :: Developers",
 		"Topic :: Office/Business",
 		"Topic :: Software Development :: Interpreters",
 		"Topic :: Software Development :: Libraries",
-		"Programming Language :: Python :: 3 :: Only",
+		
     ],
+	python_requires='>=3.7',
 )
