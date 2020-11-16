86e7ecdd4da6a68fc8ecd6ec1bbc707ff2e88eca
Ian Kjos <kjosib@gmail.com>
2020-11-15 20:45:59 -0600

    Avoid NamedTuple Multiple Inheritance
    Py3.9 has decreed that it will be an error. Cubicle only used it as a type hint. We can do this another way.

--- src/cubicle/formulae.py
+++ src/cubicle/formulae.py
@@ -15,50 +15,47 @@ FIXME: Unfortunately, such semantic checks aren't all done and some few might
 
 from typing import NamedTuple, List, Union, Dict, Container
 
-class TextElement:
-	""" ABC for things that can appear in EITHER a label OR a formula """
-
-class LiteralText(TextElement, NamedTuple):
+class LiteralText(NamedTuple):
 	text:str
 
-class PlainOrdinal(TextElement, NamedTuple):
+class PlainOrdinal(NamedTuple):
 	""" The environment provides for how to make a value "plain" (human-friendly) given both axis-name and value. """
 	axis: str
 
-class RawOrdinal(TextElement, NamedTuple):
+class RawOrdinal(NamedTuple):
 	""" In other words, the internal form of whatever ordinal. The environment need not be consulted. """
 	axis: str
 
-class Attribute(TextElement, NamedTuple):
+class Attribute(NamedTuple):
 	""" These have got to be computed. The environment must provides the method. """
 	axis: str
 	method: str
 
-class HeadRef(TextElement, NamedTuple):
+class HeadRef(NamedTuple):
 	""" For these to work, the tree-walk needs to keep track of axis header arrays by axis-name. """
 	axis: str
 	index: int
 
-class Global(TextElement, NamedTuple):
+class Global(NamedTuple):
 	""" The environment provides the corresponding (string) value. """
 	name: str
 
+# To make the IDE type-checker happy and yet remain compatible with Py3.9:
+TextElement = Union[LiteralText, PlainOrdinal, RawOrdinal, Attribute, HeadRef, Global]
 
-class Predicate:
-	""" ABC for things we know how to test... """
-
-class IsFirst(Predicate): pass
-class IsLast(Predicate): pass
-class IsEqual(Predicate, NamedTuple):
+class IsFirst: pass
+class IsLast: pass
+class IsEqual(NamedTuple):
 	distinguished_value: object
-class IsInSet(Predicate, NamedTuple):
+class IsInSet(NamedTuple):
 	including: frozenset
-class IsNotInSet(Predicate, NamedTuple):
+class IsNotInSet(NamedTuple):
 	excluding: frozenset
-class IsDefined(Predicate): pass
-class ComputedPredicate(Predicate, NamedTuple):
+class IsDefined: pass
+class ComputedPredicate(NamedTuple):
 	cookie:str
 
+Predicate = Union[IsFirst, IsLast, IsEqual, IsInSet, IsNotInSet, IsDefined, ComputedPredicate]
 
 class Selection(NamedTuple):
 	criteria: Dict[str, Predicate]
@@ -74,13 +71,13 @@ class Summation(NamedTuple):
 class BlankCell: pass
 THE_NOTHING = BlankCell()
 
-class Label(TextElement, NamedTuple):
+class Label(NamedTuple):
 	bits: List[TextElement]
 
 class Formula(NamedTuple):
-	bits: List[Union[TextElement, Selection]]
+	bits: List[Union[Selection]]
 
-class Quotation(TextElement, NamedTuple):
+class Quotation(NamedTuple):
 	""" Appropriate in formula-context containing strings. """
 	content: Label
 
