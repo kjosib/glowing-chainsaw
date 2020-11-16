e808dccadd7023f3740c2455c7ddc9cf9367266b
Ian Kjos <kjosib@gmail.com>
2020-11-15 22:02:12 -0600

    Update version.py

--- src/cubicle/version.py
+++ src/cubicle/version.py
@@ -1,3 +1,3 @@
 """ Central location for version information. """
-__version_info__ = (0, 8, 10) # Might break a few things, probably won't.
+__version_info__ = (0, 8, 11) # Might break a few things, probably won't.
 __version__ = '.'.join(map(str, __version_info__))
